73e577ed06dbc89d60ea94df613b132df47cbda0
Morgan Kleene <mkleene@virtru.com>
2024-04-29 16:49:35 -0400

    pull out all of the services

---
+++ sdk/src/main/java/io/opentdf/platform/sdk/SDKBuilder.java
@@ -0,0 +1,142 @@
+package io.opentdf.platform.sdk;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
+import com.nimbusds.oauth2.sdk.GeneralException;
+import com.nimbusds.oauth2.sdk.auth.ClientAuthentication;
+import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
+import com.nimbusds.oauth2.sdk.auth.Secret;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.Issuer;
+import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
+import io.grpc.ClientInterceptor;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.opentdf.platform.policy.attributes.AttributesServiceGrpc;
+import io.opentdf.platform.policy.namespaces.NamespaceServiceGrpc;
+import io.opentdf.platform.policy.resourcemapping.ResourceMappingServiceGrpc;
+import io.opentdf.platform.policy.subjectmapping.SubjectMappingServiceGrpc;
+import io.opentdf.platform.wellknownconfiguration.GetWellKnownConfigurationRequest;
+import io.opentdf.platform.wellknownconfiguration.GetWellKnownConfigurationResponse;
+import io.opentdf.platform.wellknownconfiguration.WellKnownServiceGrpc;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * A builder class for creating instances of the SDK class.
+ */
+public class SDKBuilder {
+    private String platformEndpoint = null;
+    private ClientAuthentication clientAuth = null;
+    private Boolean usePlainText;
+
+    public static SDKBuilder newBuilder() {
+        SDKBuilder builder = new SDKBuilder();
+        builder.usePlainText = false;
+
+        return builder;
+    }
+
+    public SDKBuilder platformEndpoint(String platformEndpoint) {
+        this.platformEndpoint = platformEndpoint;
+        return this;
+    }
+
+    public SDKBuilder withClientSecret(String clientID, String clientSecret) {
+        ClientID cid = new ClientID(clientID);
+        Secret cs = new Secret(clientSecret);
+        this.clientAuth = new ClientSecretBasic(cid, cs);
+        return this;
+    }
+
+    public SDKBuilder withPlainTextConnection(Boolean usePlainText) {
+        this.usePlainText = usePlainText;
+        return this;
+    }
+
+    public SDK build() {
+
+        ManagedChannelBuilder<?> bootstrapChannelBuilder = getManagedChannelBuilder();
+
+        GetWellKnownConfigurationResponse config;
+        try {
+            config = WellKnownServiceGrpc
+                    .newBlockingStub(bootstrapChannelBuilder.build())
+                    .getWellKnownConfiguration(GetWellKnownConfigurationRequest.getDefaultInstance());
+        } catch (Exception e) {
+            Status status = Status.fromThrowable(e);
+            throw new RuntimeException(String.format("Got grpc status [%s] when getting configuration", status), e);
+        }
+
+        String platformIssuer;
+        try {
+            platformIssuer = config
+                    .getConfiguration()
+                    .getFieldsOrThrow(SDK.PLATFORM_ISSUER)
+                    .getStringValue();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error getting the issuer from the platform", e);
+        }
+
+        Issuer issuer = new Issuer(platformIssuer);
+        OIDCProviderMetadata providerMetadata;
+        try {
+            providerMetadata = OIDCProviderMetadata.resolve(issuer);
+        } catch (IOException | GeneralException e) {
+            throw new RuntimeException("Error resolving the OIDC provider metadata", e);
+        }
+
+        RSAKey rsaKey;
+        try {
+            rsaKey = new RSAKeyGenerator(2048)
+                    .keyUse(KeyUse.SIGNATURE)
+                    .keyID(UUID.randomUUID().toString())
+                    .generate();
+        } catch (JOSEException e) {
+            throw new RuntimeException("Error generating DPoP key", e);
+        }
+
+        ClientInterceptor interceptor = new GRPCAuthInterceptor(clientAuth, rsaKey, providerMetadata.getTokenEndpointURI());
+        ManagedChannel channel = getManagedChannelBuilder()
+                .intercept(interceptor)
+                .build();
+
+        SDK.Services services = new SDK.Services() {
+            final AttributesServiceGrpc.AttributesServiceFutureStub attributes = AttributesServiceGrpc.newFutureStub(channel);
+            final NamespaceServiceGrpc.NamespaceServiceFutureStub namespaces = NamespaceServiceGrpc.newFutureStub(channel);
+            final SubjectMappingServiceGrpc.SubjectMappingServiceFutureStub subjectMappings = SubjectMappingServiceGrpc.newFutureStub(channel);
+            final ResourceMappingServiceGrpc.ResourceMappingServiceFutureStub resourceMappings = ResourceMappingServiceGrpc.newFutureStub(channel);
+            @Override
+            public AttributesServiceGrpc.AttributesServiceFutureStub attributes() {
+                return attributes;
+            }
+            @Override
+            public NamespaceServiceGrpc.NamespaceServiceFutureStub namespaces() {
+                return namespaces;
+            }
+            @Override
+            public SubjectMappingServiceGrpc.SubjectMappingServiceFutureStub subjectMappings() {
+                return subjectMappings;
+            }
+            @Override
+            public ResourceMappingServiceGrpc.ResourceMappingServiceFutureStub resourceMappings() {
+                return resourceMappings;
+            }
+        };
+        return new SDK(services);
+    }
+    private ManagedChannelBuilder<?> getManagedChannelBuilder() {
+        ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder
+                .forTarget(platformEndpoint);
+
+        if (usePlainText) {
+            channelBuilder = channelBuilder.usePlaintext();
+        }
+        return channelBuilder;
+    }
+}
