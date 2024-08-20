276c26fa63c7fb07be740a0dea6c4c372c2649a1
Timothy Tschampel <ttschampel@virtru.com>
2024-08-20 12:05:45 -0700

    create stubs once

--- sdk/src/main/java/io/opentdf/platform/sdk/AttributesClient.java
+++ sdk/src/main/java/io/opentdf/platform/sdk/AttributesClient.java
@@ -1,14 +1,15 @@
 package io.opentdf.platform.sdk;
 
 import io.grpc.ManagedChannel;
-import io.opentdf.platform.policy.attributes.GetAttributeValuesByFqnsRequest;
 import io.opentdf.platform.policy.attributes.AttributesServiceGrpc;
-import io.opentdf.platform.policy.attributes.GetAttributeValuesByFqnsResponse;
-
 
 public class AttributesClient implements SDK.AttributesService {
 
     private final ManagedChannel channel;
+
+    private AttributesServiceGrpc.AttributesServiceBlockingStub attributesServiceBlockingStub;
+    private AttributesServiceGrpc.AttributesServiceStub attributesServiceStub;
+    private AttributesServiceGrpc.AttributesServiceFutureStub attributesServiceFutureStub;
 
     /***
      * A client that communicates with Attributes Service
@@ -24,17 +25,26 @@ public class AttributesClient implements SDK.AttributesService {
     }
 
     @Override
-    public AttributesServiceGrpc.AttributesServiceBlockingStub getAttributesServiceBlockingStub() {
-        return AttributesServiceGrpc.newBlockingStub(channel);
+    public synchronized AttributesServiceGrpc.AttributesServiceBlockingStub getAttributesServiceBlockingStub() {
+        if (attributesServiceBlockingStub==null){
+            attributesServiceBlockingStub = AttributesServiceGrpc.newBlockingStub(channel);
+        }
+        return attributesServiceBlockingStub;
     }
 
     @Override
-    public AttributesServiceGrpc.AttributesServiceStub getAttributesServiceStub() {
-        return AttributesServiceGrpc.newStub(channel);
+    public synchronized AttributesServiceGrpc.AttributesServiceStub getAttributesServiceStub() {
+        if (attributesServiceStub==null){
+            attributesServiceStub = AttributesServiceGrpc.newStub(channel);
+        }
+        return attributesServiceStub;
     }
 
     @Override
-    public AttributesServiceGrpc.AttributesServiceFutureStub getAttributesServiceFutureStub() {
-        return AttributesServiceGrpc.newFutureStub(channel);
+    public synchronized AttributesServiceGrpc.AttributesServiceFutureStub getAttributesServiceFutureStub() {
+        if (attributesServiceFutureStub==null){
+            attributesServiceFutureStub = AttributesServiceGrpc.newFutureStub(channel);
+        }
+        return attributesServiceFutureStub;
     }
 }
