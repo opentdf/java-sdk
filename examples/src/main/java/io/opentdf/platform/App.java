5f1401437a10f69158818d8eb73e5548ce97a30b
Elizabeth Healy <ehealy@virtru.com>
2024-10-24 14:39:42 -0400

    create examples

---
+++ examples/src/main/java/io/opentdf/platform/App.java
@@ -0,0 +1,7 @@
+package io.opentdf.platform;
+
+public class App {
+    public static void main(String[] args) throws ExecutionException, InterruptedException{
+        System.out.println("opentdf examples");
+    }
+}
