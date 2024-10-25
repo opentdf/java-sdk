902d0addea23997186fed3a60290e67223db5354
Elizabeth Healy <ehealy@virtru.com>
2024-10-25 11:07:05 -0400

    example module working

--- examples/src/main/java/io/opentdf/platform/App.java
+++ examples/src/main/java/io/opentdf/platform/App.java
@@ -1,7 +1,7 @@
 package io.opentdf.platform;
 
 public class App {
-    public static void main(String[] args) throws ExecutionException, InterruptedException{
+    public static void main(String[] args) {
         System.out.println("opentdf examples");
     }
 }
