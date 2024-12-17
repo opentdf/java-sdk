b2bc54026abc1f65b3c439470c0b9131d4089c15
sujan kota <sujankota@gmail.com>
2024-12-17 09:12:03 -0500

    fix the build

--- cmdline/src/main/java/io/opentdf/platform/Command.java
+++ cmdline/src/main/java/io/opentdf/platform/Command.java
@@ -69,7 +69,7 @@ class Command {
             @Option(names = { "--mime-type" }, defaultValue = Option.NULL_VALUE) Optional<String> mimeType,
             @Option(names = { "--with-assertions" }, defaultValue = Option.NULL_VALUE) Optional<String> assertion)
 
-            throws IOException, JOSEException, AutoConfigureException, InterruptedException, ExecutionException {
+            throws IOException, JOSEException, AutoConfigureException, InterruptedException, ExecutionException, DecoderException {
 
         var sdk = buildSDK();
         var kasInfos = kas.stream().map(k -> {
