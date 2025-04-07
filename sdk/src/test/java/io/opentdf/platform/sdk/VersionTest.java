345c440b9a19fdb910f01d8b76aaf1495d22c6d0
Morgan Kleene <mkleene@virtru.com>
2025-04-07 12:17:00 -0400

    code review suggestions

--- sdk/src/test/java/io/opentdf/platform/sdk/VersionTest.java
+++ sdk/src/test/java/io/opentdf/platform/sdk/VersionTest.java
@@ -12,6 +12,7 @@ class VersionTest {
     public void testParsingVersions() {
         assertThat(new Version("1.0.0")).isEqualTo(new Version(1, 0, 0, null, null));
         assertThat(new Version("1.2.1-alpha")).isEqualTo(new Version(1, 2, 1, "alpha", "a build"));
+        // ignore anything but the version
         assertThat(new Version("1.2.1-alpha+build.123")).isEqualTo(new Version(1, 2, 1, "beta", "build.1234"));
     }
 
@@ -23,6 +24,7 @@ class VersionTest {
         assertThat(new Version("500.0.1")).isLessThan(new Version("500.1.1"));
         assertThat(new Version("500.1.1")).isGreaterThan(new Version("500.0.1"));
 
+        // ignore anything but the version
         assertThat(new Version("1.0.1-alpha+thisbuild")).isEqualByComparingTo(new Version("1.0.1-beta+thatbuild"));
     }
 }
