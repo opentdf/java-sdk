d7940c47981d8b641ee63e6cadf9fd083a669841
Morgan Kleene <mkleene@virtru.com>
2025-04-30 10:01:20 -0400

    better comment

--- sdk/src/main/kotlin/io/opentdf/platform/sdk/RequestHelper.kt
+++ sdk/src/main/kotlin/io/opentdf/platform/sdk/RequestHelper.kt
@@ -7,9 +7,11 @@ import com.connectrpc.getOrThrow
 class RequestHelper {
     companion object {
         /**
-         * Kotlin doesn't have checked exceptions so that if we want to catch
-         * an exception thrown by Kotlin we need to declare that the method throws it.
-         * ResponseMessageKt.getOrThrow() doesn't do so; so we need to wrap it
+         * Kotlin doesn't have checked exceptions (importantly it doesn't declare them).
+         *  This means that if a Kotlin function throws a checked exception, you can't
+         *  catch it in Java unless it uses the {@class kotlin.jvm.Throws} annotation.
+         *  We wrap the getOrThrow() method in a static method with the annotation we
+         *  need to catch a {@class ConnectException} in Java.
          */
         @JvmStatic @Throws(ConnectException::class)
         fun <T> getOrThrow(responseMessage: ResponseMessage<T>): T {
