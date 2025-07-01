98653b9bc4f05cd37ef8593231e1d4916515c1a6
Morgan Kleene <mkleene@virtru.com>
2025-07-01 18:11:04 +0200

    restructure

--- sdk/src/main/java/io/opentdf/platform/sdk/Planner.java
+++ sdk/src/main/java/io/opentdf/platform/sdk/Planner.java
@@ -9,43 +9,67 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
+
+import static io.opentdf.platform.sdk.Autoconfigure.Granter.generatePlanFromDefaultKases;
 
 public class Planner {
     private final Config.TDFConfig tdfConfig;
     private final SDK.Services sdkServices;
-    private final Autoconfigure.Granter granter;
 
     private static final Logger logger = LoggerFactory.getLogger(Planner.class);
 
     public Planner(Config.TDFConfig config, SDK.Services services) {
         tdfConfig = Objects.requireNonNull(config);
         sdkServices = Objects.requireNonNull(services) ;
+    }
 
-        List<String> dk = defaultKases(tdfConfig);
+    private static String getUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    Map<String, List<Config.KASInfo>> getSplits(Config.TDFConfig tdfConfig) {
+        List<Autoconfigure.KeySplitStep> splitPlan;
+        List<String> defaultKases = defaultKases(tdfConfig);
         if (tdfConfig.autoconfigure) {
-            if (tdfConfig.splitPlan != null && !tdfConfig.splitPlan.isEmpty()) {
-                throw new IllegalArgumentException("cannot use autoconfigure with a split plan provided in the TDFConfig");
-            }
-            Autoconfigure.Granter granter = new Autoconfigure.Granter(new ArrayList<>());
-            if (tdfConfig.attributeValues != null && !tdfConfig.attributeValues.isEmpty()) {
-                granter = Autoconfigure.newGranterFromAttributes(services.kas().getKeyCache(), tdfConfig.attributeValues.toArray(new Value[0]));
-            } else if (tdfConfig.attributes != null && !tdfConfig.attributes.isEmpty()) {
-                granter = Autoconfigure.newGranterFromService(services.attributes(), services.kas().getKeyCache(),
-                        tdfConfig.attributes.toArray(new Autoconfigure.AttributeValueFQN[0]));
-            }
-
-            this.granter = granter;
+            splitPlan = getAutoconfigurePlan(tdfConfig);
+        } else if (tdfConfig.splitPlan == null || tdfConfig.splitPlan.isEmpty()) {
+            splitPlan = defaultKases.isEmpty()
+                    ? createPlanFromBaseKey()
+                    : generatePlanFromDefaultKases(defaultKases, Planner::getUUID);
         } else {
-            this.granter = null;
+            splitPlan = tdfConfig.splitPlan;
         }
 
         if (tdfConfig.kasInfoList.isEmpty() && tdfConfig.splitPlan.isEmpty()) {
             throw new SDK.KasInfoMissing("kas information is missing, no key access template specified or inferred");
         }
+
+        // split plan: restructure by conjunctions
+        return fillInKeys(tdfConfig, splitPlan);
     }
 
-    Map<String, List<Config.KASInfo>> getSplits(Config.TDFConfig tdfConfig) {
+    private List<Autoconfigure.KeySplitStep> getAutoconfigurePlan(Config.TDFConfig tdfConfig) {
+        if (tdfConfig.splitPlan != null && !tdfConfig.splitPlan.isEmpty()) {
+            throw new IllegalArgumentException("cannot use autoconfigure with a split plan provided in the TDFConfig");
+        }
+        Autoconfigure.Granter granter = new Autoconfigure.Granter(new ArrayList<>());
+        if (tdfConfig.attributeValues != null && !tdfConfig.attributeValues.isEmpty()) {
+            granter = Autoconfigure.newGranterFromAttributes(sdkServices.kas().getKeyCache(), tdfConfig.attributeValues.toArray(new Value[0]));
+        } else if (tdfConfig.attributes != null && !tdfConfig.attributes.isEmpty()) {
+            granter = Autoconfigure.newGranterFromService(sdkServices.attributes(), sdkServices.kas().getKeyCache(),
+                    tdfConfig.attributes.toArray(new Autoconfigure.AttributeValueFQN[0]));
+        }
+        return granter.getSplits(defaultKases(tdfConfig), Planner::getUUID, Optional::empty);
+    }
+
+    private List<Autoconfigure.KeySplitStep> createPlanFromBaseKey() {
+        return null;
+    }
+
+    private Map<String, List<Config.KASInfo>> fillInKeys(Config.TDFConfig tdfConfig, List<Autoconfigure.KeySplitStep> splitPlan) {
+        Map<String, List<Config.KASInfo>> conjunction = new HashMap<>();
         var latestKASInfo = new HashMap<String, Config.KASInfo>();
         // Seed anything passed in manually
         for (Config.KASInfo kasInfo : tdfConfig.kasInfoList) {
@@ -54,10 +78,7 @@ public class Planner {
             }
         }
 
-        // split plan: restructure by conjunctions
-        Map<String, List<Config.KASInfo>> conjunction = new HashMap<>();
-
-        for (Autoconfigure.KeySplitStep splitInfo : tdfConfig.splitPlan) {
+        for (Autoconfigure.KeySplitStep splitInfo: splitPlan) {
             // Public key was passed in with kasInfoList
             // TODO First look up in attribute information / add to split plan?
             Config.KASInfo ki = latestKASInfo.get(splitInfo.kas);
