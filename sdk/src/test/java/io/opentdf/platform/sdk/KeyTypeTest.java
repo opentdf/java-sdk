4e3b907366c608ed556eabeb8f26e363e2e2a5af
Morgan Kleene <mkleene@virtru.com>
2025-07-22 22:27:39 +0200

    fix test

--- sdk/src/test/java/io/opentdf/platform/sdk/KeyTypeTest.java
+++ sdk/src/test/java/io/opentdf/platform/sdk/KeyTypeTest.java
@@ -1,11 +1,15 @@
 package io.opentdf.platform.sdk;
 
-import io.opentdf.platform.policy.Algorithm;
 import org.junit.jupiter.api.Test;
 
+import static io.opentdf.platform.policy.Algorithm.ALGORITHM_EC_P256;
+import static io.opentdf.platform.policy.Algorithm.ALGORITHM_EC_P384;
+import static io.opentdf.platform.policy.Algorithm.ALGORITHM_EC_P521;
+import static io.opentdf.platform.policy.Algorithm.ALGORITHM_RSA_2048;
 import static io.opentdf.platform.policy.KasPublicKeyAlgEnum.KAS_PUBLIC_KEY_ALG_ENUM_EC_SECP256R1;
 import static io.opentdf.platform.policy.KasPublicKeyAlgEnum.KAS_PUBLIC_KEY_ALG_ENUM_EC_SECP384R1;
 import static io.opentdf.platform.policy.KasPublicKeyAlgEnum.KAS_PUBLIC_KEY_ALG_ENUM_EC_SECP521R1;
+import static io.opentdf.platform.policy.KasPublicKeyAlgEnum.KAS_PUBLIC_KEY_ALG_ENUM_RSA_2048;
 import static org.junit.jupiter.api.Assertions.*;
 
 class KeyTypeTest {
@@ -24,15 +28,15 @@ class KeyTypeTest {
 
     @Test
     void testFromAlgorithm() {
-        assertEquals(KeyType.RSA2048Key, KeyType.fromAlgorithm(Algorithm.ALGORITHM_RSA_2048));
-        assertEquals(KeyType.EC256Key, KeyType.fromAlgorithm(Algorithm.ALGORITHM_EC_P256));
-        assertEquals(KeyType.EC384Key, KeyType.fromAlgorithm(Algorithm.ALGORITHM_EC_P384));
-        assertEquals(KeyType.EC521Key, KeyType.fromAlgorithm(Algorithm.ALGORITHM_EC_P521));
+        assertEquals(KeyType.RSA2048Key, KeyType.fromAlgorithm(ALGORITHM_RSA_2048));
+        assertEquals(KeyType.EC256Key, KeyType.fromAlgorithm(ALGORITHM_EC_P256));
+        assertEquals(KeyType.EC384Key, KeyType.fromAlgorithm(ALGORITHM_EC_P384));
+        assertEquals(KeyType.EC521Key, KeyType.fromAlgorithm(ALGORITHM_EC_P521));
     }
 
     @Test
     void testFromPublicKeyAlgEnum() {
-        assertEquals(KeyType.RSA2048Key, KeyType.fromPublicKeyAlgorithm(KAS_PUBLIC_KEY_ALG_ENUM_EC_SECP256R1));
+        assertEquals(KeyType.RSA2048Key, KeyType.fromPublicKeyAlgorithm(KAS_PUBLIC_KEY_ALG_ENUM_RSA_2048));
         assertEquals(KeyType.EC256Key, KeyType.fromPublicKeyAlgorithm(KAS_PUBLIC_KEY_ALG_ENUM_EC_SECP256R1));
         assertEquals(KeyType.EC384Key, KeyType.fromPublicKeyAlgorithm(KAS_PUBLIC_KEY_ALG_ENUM_EC_SECP384R1));
         assertEquals(KeyType.EC521Key, KeyType.fromPublicKeyAlgorithm(KAS_PUBLIC_KEY_ALG_ENUM_EC_SECP521R1));
