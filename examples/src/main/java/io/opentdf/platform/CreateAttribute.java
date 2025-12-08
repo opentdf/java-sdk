2e5660c50b7c5fd80ad4137c1f17a1e1f4470588
Scott Hamrick <2623452+cshamrick@users.noreply.github.com>
2025-12-08 15:07:43 -0600

    fix(examples): improve logging and setup logic in examples (#316)
    Signed-off-by: Scott Hamrick <2623452+cshamrick@users.noreply.github.com>

--- examples/src/main/java/io/opentdf/platform/CreateAttribute.java
+++ examples/src/main/java/io/opentdf/platform/CreateAttribute.java
@@ -1,37 +1,80 @@
 package io.opentdf.platform;
+
 import com.connectrpc.ResponseMessageKt;
-import io.opentdf.platform.policy.Attribute;
 import io.opentdf.platform.policy.AttributeRuleTypeEnum;
+import io.opentdf.platform.policy.Namespace;
 import io.opentdf.platform.policy.attributes.CreateAttributeRequest;
 import io.opentdf.platform.policy.attributes.CreateAttributeResponse;
+import io.opentdf.platform.policy.namespaces.GetNamespaceRequest;
 import io.opentdf.platform.sdk.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Collections;
-import java.util.concurrent.ExecutionException;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class CreateAttribute {
-    public static void main(String[] args) {
 
-        String clientId = "opentdf";
-        String clientSecret = "secret";
-        String platformEndpoint = "localhost:8080";
+  private static final Logger logger = LogManager.getLogger(CreateAttribute.class);
 
-        SDKBuilder builder = new SDKBuilder();
-        SDK sdk = builder.platformEndpoint(platformEndpoint)
-                .clientSecret(clientId, clientSecret).useInsecurePlaintextConnection(true)
-                .build();
+  public static void main(String[] args) {
 
-        CreateAttributeRequest request = CreateAttributeRequest.newBuilder()
-        .setNamespaceId("877990d1-609b-42ab-a273-4253b8b321eb")
-        .setName("test")
-        .setRule(AttributeRuleTypeEnum.forNumber(AttributeRuleTypeEnum.ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF_VALUE))
-        .addAllValues(Arrays.asList("test1", "test2")).build();
+    String clientId = "opentdf";
+    String clientSecret = "secret";
+    String platformEndpoint = "localhost:8080";
+    String namespaceName = "opentdf.io";
 
-        CreateAttributeResponse resp = ResponseMessageKt.getOrThrow(sdk.getServices().attributes().createAttributeBlocking(request, Collections.emptyMap()).execute());
+    SDKBuilder builder = new SDKBuilder();
 
-        Attribute attribute = resp.getAttribute();
+    try (SDK sdk =
+        builder
+            .platformEndpoint(platformEndpoint)
+            .clientSecret(clientId, clientSecret)
+            .useInsecurePlaintextConnection(true)
+            .build()) {
 
+      Namespace namespace =
+          ResponseMessageKt.getOrThrow(
+                  sdk.getServices()
+                      .namespaces()
+                      .getNamespaceBlocking(
+                          GetNamespaceRequest.newBuilder()
+                              .setFqn("https://" + namespaceName)
+                              .build(),
+                          Collections.emptyMap())
+                      .execute())
+              .getNamespace();
+
+      CreateAttributeRequest createAttributeRequest =
+          CreateAttributeRequest.newBuilder()
+              .setNamespaceId(namespace.getId())
+              .setName("test-attribute")
+              .setRule(
+                  AttributeRuleTypeEnum.forNumber(
+                      AttributeRuleTypeEnum.ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF_VALUE))
+              .addAllValues(Arrays.asList("test1", "test2"))
+              .build();
+
+      CreateAttributeResponse createAttributeResponse =
+          ResponseMessageKt.getOrThrow(
+              sdk.getServices()
+                  .attributes()
+                  .createAttributeBlocking(createAttributeRequest, Collections.emptyMap())
+                  .execute());
+
+      logger.info(
+          "Successfully created attribute with ID: {}",
+          createAttributeResponse.getAttribute().getId());
+    } catch (Exception e) {
+      if (Objects.equals(e.getMessage(), "resource not found")) {
+        logger.error("Namespace '{}' not found", namespaceName, e);
+      } else if (Objects.equals(e.getMessage(), "resource unique field violation")) {
+        logger.error("Attribute already exists", e);
+      } else {
+        logger.error("Failed to create attribute", e);
+      }
     }
+  }
 }
--- examples/src/main/java/io/opentdf/platform/GetDecisions.java
+++ examples/src/main/java/io/opentdf/platform/GetDecisions.java
@@ -1,4 +1,5 @@
 package io.opentdf.platform;
+
 import com.connectrpc.ResponseMessageKt;
 import io.opentdf.platform.authorization.DecisionRequest;
 import io.opentdf.platform.authorization.DecisionResponse;
@@ -9,36 +10,68 @@ import io.opentdf.platform.authorization.GetDecisionsResponse;
 import io.opentdf.platform.authorization.ResourceAttribute;
 import io.opentdf.platform.policy.Action;
 import io.opentdf.platform.sdk.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Collections;
-import java.util.concurrent.ExecutionException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GetDecisions {
-    public static void main(String[] args) {
 
-        String clientId = "opentdf";
-        String clientSecret = "secret";
-        String platformEndpoint = "localhost:8080";
+  private static final Logger logger = LogManager.getLogger(GetDecisions.class);
 
-        SDKBuilder builder = new SDKBuilder();
-        SDK sdk = builder.platformEndpoint(platformEndpoint)
-                .clientSecret(clientId, clientSecret).useInsecurePlaintextConnection(true)
-                .build();
-    
-        GetDecisionsRequest request = GetDecisionsRequest.newBuilder()
-        .addDecisionRequests(DecisionRequest.newBuilder()
-        .addEntityChains(EntityChain.newBuilder().setId("ec1").addEntities(Entity.newBuilder().setId("entity-1").setClientId("opentdf")))
-        .addActions(Action.newBuilder().setStandard(Action.StandardAction.STANDARD_ACTION_DECRYPT))
-        .addResourceAttributes(ResourceAttribute.newBuilder().setResourceAttributesId("resource-attribute-1")
-            .addAttributeValueFqns("https://mynamespace.com/attr/test/value/test1"))
-        ).build();
+  public static void main(String[] args) {
 
-        GetDecisionsResponse resp = ResponseMessageKt.getOrThrow(sdk.getServices().authorization().getDecisionsBlocking(request, Collections.emptyMap()).execute());
+    String clientId = "opentdf";
+    String clientSecret = "secret";
+    String platformEndpoint = "localhost:8080";
+    String namespaceName = "opentdf.io";
 
-        List<DecisionResponse> decisions = resp.getDecisionResponsesList();
+    SDKBuilder builder = new SDKBuilder();
 
-        System.out.println(DecisionResponse.Decision.forNumber(decisions.get(0).getDecisionValue()));
+    try (SDK sdk =
+        builder
+            .platformEndpoint(platformEndpoint)
+            .clientSecret(clientId, clientSecret)
+            .useInsecurePlaintextConnection(true)
+            .build()) {
+
+      GetDecisionsRequest request =
+          GetDecisionsRequest.newBuilder()
+              .addDecisionRequests(
+                  DecisionRequest.newBuilder()
+                      .addEntityChains(
+                          EntityChain.newBuilder()
+                              .setId("ec1")
+                              .addEntities(
+                                  Entity.newBuilder().setId("entity-1").setClientId("opentdf")))
+                      .addActions(Action.newBuilder().setName("read"))
+                      .addResourceAttributes(
+                          ResourceAttribute.newBuilder()
+                              .setResourceAttributesId("resource-attribute-1")
+                              .addAttributeValueFqns(
+                                  "https://" + namespaceName + "/attr/test/value/test1")))
+              .build();
+
+      GetDecisionsResponse getDecisionsResponse =
+          ResponseMessageKt.getOrThrow(
+              sdk.getServices()
+                  .authorization()
+                  .getDecisionsBlocking(request, Collections.emptyMap())
+                  .execute());
+
+      List<DecisionResponse> decisions = getDecisionsResponse.getDecisionResponsesList();
+
+      logger.info(
+          "Successfully retrieved decisions: [{}]",
+          decisions.stream()
+              .map(DecisionResponse::getDecision)
+              .map(DecisionResponse.Decision::toString)
+              .collect(Collectors.joining(", ")));
+    } catch (Exception e) {
+      logger.error("Failed to get decisions", e);
     }
+  }
 }
