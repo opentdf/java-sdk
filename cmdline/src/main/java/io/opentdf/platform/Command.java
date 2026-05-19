89fa4b24c403c4f21d376e6e37c2e33d2e9ceeaf
Morgan Kleene <mkleene@virtru.com>
2026-05-19 15:27:46 -0400

    code review feedback

--- cmdline/src/main/java/io/opentdf/platform/Command.java
+++ cmdline/src/main/java/io/opentdf/platform/Command.java
@@ -263,13 +263,7 @@ class Command {
     private SDK buildSDK() {
         SDKBuilder builder = new SDKBuilder();
         if (insecure) {
-            // Trust all certificates
-            X509TrustManager insecureTrustManager = new X509TrustManager() {
-                @Override public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-                @Override public void checkServerTrusted(X509Certificate[] chain, String authType) {}
-                @Override public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-            };
-            builder.sslFactoryFromTrustManager(insecureTrustManager);
+            builder.insecureSslFactory();
         }
 
         return builder.platformEndpoint(platformEndpoint)
