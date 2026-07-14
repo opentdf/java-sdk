2f1d2e4a27ec325e385d8b635677f3fdd4fc47d9
Dave Mihalcik <dmihalcik@virtru.com>
2026-07-14 06:51:28 -0400

    chore(sdk): remove unused imports flagged by SonarCloud
    Drops orphaned imports that SonarCloud (java:S1128) reported:
    - SDKBuilder: com.nimbusds.jose.jwk.Curve, com.nimbusds.jose.jwk.gen.ECKeyGenerator
    - TokenSource: com.nimbusds.jose.jwk.RSAKey

--- sdk/src/main/java/io/opentdf/platform/sdk/TokenSource.java
+++ sdk/src/main/java/io/opentdf/platform/sdk/TokenSource.java
@@ -3,7 +3,6 @@ package io.opentdf.platform.sdk;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.oauth2.sdk.AuthorizationGrant;
 import com.nimbusds.oauth2.sdk.ErrorObject;
